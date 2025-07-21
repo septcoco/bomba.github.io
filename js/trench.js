fcac12778c808808da4af2055b6697c58fe13a9f
Turnstiles <animeister10@gmail.com>
2025-07-21 11:36:13 -0400

    aaahgaha

---
+++ js/trench.js
@@ -0,0 +1,29 @@
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+window.onload = function () {
+  const visited = getCookie("visited");
+  const welcomeMessage = document.getElementById("welcomeMessage");
+
+  if (visited) {
+    welcomeMessage.textContent = "Welcome back to Trench";
+  } else {
+    welcomeMessage.textContent = "Welcome to Trench";
+    setCookie("visited", "true", 365);
+  }
+};
